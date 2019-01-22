00787bd7e6bf10c9b3e62804458fa0d63a67b6bb
Zheng Wang <Zheng.Wang@feisystems.com>
2019-01-22 16:08:36 +0800

    validate the ajax request and DDL bind

--- TestMVCDay1/TestMVCDay1/Controllers/TestController.cs
+++ TestMVCDay1/TestMVCDay1/Controllers/TestController.cs
@@ -68,18 +68,48 @@ namespace TestMVCDay1.Controllers
             list.Add(new Models.Person { Id = 2, Name = "us" });
             list.Add(new Models.Person { Id = 3, Name = "en" });
             list.Add(new Models.Person { Id = 4, Name = "un" });
+            /*1.
+                        List<SelectListItem> sliList = new List<SelectListItem>();
+                        foreach (var item in list)
+                        {
+                            SelectListItem listItem = new SelectListItem();
+                            listItem.Selected = (item.Id == 2);
+                            listItem.Text = item.Name;
+                            listItem.Value = item.Id.ToString();
+                            sliList.Add(listItem);
+                        }
+                        return View(sliList);
+                        */
+            /*2  var sList = from item in list
+                          select new SelectListItem { Selected = item.Id == 2, Text = item.Name, Value = item.Id.ToString() };
+                          return View(sList);
+                          */
+            SelectList selList = new SelectList(list, "Id", "Name");
+            ViewBag.selList = selList;
 
-            List<SelectListItem> sliList = new List<SelectListItem>();
-            foreach (var item in list)
+            return View();
+
+        }
+        public ActionResult AjaxTest()
+        {
+            Person p = new Person();
+            p.Name = "zheng";
+            //主流浏览器在发出ajax请求的时候都会带着
+            //X -Requested-With:XMLHttpRequest报文头
+            if (Request.IsAjaxRequest())
             {
-                SelectListItem listItem = new SelectListItem();
-                listItem.Selected = (item.Id == 2);
-                listItem.Text = item.Name;
-                listItem.Value = item.Id.ToString();
-                sliList.Add(listItem);
-            }
-            return View(sliList);
 
+                return Json(p);
+            }
+            else
+            {
+                return Content(p.Name);
+            }
+
+        }
+        public ActionResult TypeHttpUrlTest()
+        {
+            return View();
         }
     }
 }
