6f1c1d3a0225c1309d778e1ca6030d3e29475ed1
Zheng Wang <Zheng.Wang@feisystems.com>
2019-03-27 09:54:41 +0800

    拆箱

---
+++ sort of knowledge/拆箱装箱/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace 拆箱装箱
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //拆箱的时候一定要用装箱的类型
+            /*
+            int i = 5;
+            object obj1 = i;
+            long j2 = i;
+            long j3 = (long)i;
+            long j4 = (long)obj1;//会报错
+            object  ExecuteScalar("select count(*) from t") long int
+            int i=Convert.toInt32(obj1)
+            */
+            
+
+            int i = 5;
+            object obj = i;//object C#,Object CTS 装箱（boxing）
+            int i1 = (int)obj;//拆箱
+            Console.WriteLine(i1);
+            Console.ReadKey();
+        }
+    }
+}
