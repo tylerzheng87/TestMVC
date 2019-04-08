28f632140880862f3eda0826c54c352f948b9085
Zheng Wang <Zheng.Wang@feisystems.com>
2019-04-08 10:29:38 +0800

    事件入门

---
+++ sort of knowledge/事件入门/Person.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace 事件入门
+{
+    public class Person
+    {
+        private int age;
+        public int Age
+        {
+            get
+            {
+                return this.age;
+            }
+            set
+            {
+                this.age = value;
+                if (value % 12 == 0)
+                {
+                    //if (OnBenMingNian != null)
+                    //{
+                    //    OnBenMingNian();//调用添加的组合方法
+                    //}
+
+                    if (this._OnBenMingNian!=null)
+                    {
+                        this._OnBenMingNian();
+                    }
+                }
+            }
+        }
+        public  Action _OnBenMingNian;
+        //public event Action OnBenmingNian;//event 委托类型 事件的名字简化写法
+        //  public  Action OnBenmingNian;//event 委托类型 事件的名字
+
+            //事件是私有的委托和add remove构成的 这是与委托的本质区别
+        public event Action OnBenMingNian
+        {
+            add
+            {
+
+                this._OnBenMingNian += value;
+
+            }
+            remove
+            {
+                this._OnBenMingNian -= value;
+            }
+
+        }
+    }
+}
---
+++ sort of knowledge/事件入门/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace 事件入门
+{
+    class Program
+    {
+       
+        static void Main(string[] args)
+        {
+            Person p1 = new Person();
+            p1.OnBenMingNian += BMN;//  p1.OnBenmingNian=  p1.OnBenmingNian+BMN2;
+            p1.OnBenMingNian -= BMN2;
+            //加event不能让他指向null这就是event和委托的区别    p1.OnBenmingNian = null;
+            p1.Age = 5;
+            Console.WriteLine(p1.Age);
+            p1.Age = 24;
+            Console.WriteLine(p1.Age);
+            p1.Age = 55;
+            Console.WriteLine(p1.Age);
+            p1.Age = 48;
+            Console.WriteLine(p1.Age);
+            Console.ReadKey();
+        }
+
+        private static void BMN2()
+        {
+            Console.WriteLine("22222222到了本命年了");
+        }
+
+        private static void BMN()
+        {
+            Console.WriteLine("到了本命年了");
+        }
+    }
+}
