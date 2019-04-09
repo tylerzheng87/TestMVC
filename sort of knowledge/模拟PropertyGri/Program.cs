2d3927c9a32912aaf4b855cec42bea13a6f86dea
Zheng Wang <Zheng.Wang@feisystems.com>
2019-04-09 10:54:19 +0800

    模拟PropertyGrid

---
+++ sort of knowledge/模拟PropertyGri/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace 模拟PropertyGri
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person p1 = new Person();
+            p1.Age = 18;
+            p1.Name = "Tman";
+            ShowObject(p1);
+            Console.ReadKey();
+        }
+
+
+        static void ShowObject(object obj)
+        {
+            Type type = obj.GetType();
+            PropertyInfo[] props = type.GetProperties();
+            foreach (var prop in props)
+            {
+                if (prop.CanRead)
+                {
+                    string propName = prop.Name;
+                    object value = prop.GetValue(obj);//获取obj对象的prop属性的值
+                    Console.WriteLine(propName + "=" + value);
+                }
+            }
+        }
+    }
+    class Person : Program
+    {
+        public Person()
+        {
+
+        }
+        public Person(string name)
+        {
+
+
+        }
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public void SayHi()
+        {
+            Console.WriteLine("大家好,我是" + this.Name + "，我的年龄" + this.Age);
+        }
+    }
+}
