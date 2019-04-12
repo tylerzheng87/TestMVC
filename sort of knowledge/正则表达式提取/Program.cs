6aecdce1db48976d6a815f3e217b1783b2f05a3a
Zheng Wang <Zheng.Wang@feisystems.com>
2019-04-12 11:33:34 +0800

    正则表达式的提取

---
+++ sort of knowledge/正则表达式提取/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace 正则表达式提取
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //使用()获取“分组”
+            Match match = Regex.Match("2008-8-08", @"^(\d{4})\-(\d{1,2})\-(\d{1,2})$");
+            if (match.Success)
+            {
+                string year = match.Groups[1].Value;
+                string month = match.Groups[2].Value;
+                string day = match.Groups[3].Value;
+                Console.WriteLine("年" + year + "月" + month + "日" + day);
+            }
+            else
+            {
+                Console.WriteLine("匹配失败");
+            }
+            Console.ReadKey();
+        }
+    }
+}
