5f13d702786a638e5715b32041e60721e30c89d5
tylerzheng87 <15114565716@163.com>
2020-05-04 19:16:43 +0800

    完成飞行棋

--- TestMVCDay1/LittleGameAirGuess/Program.cs
+++ TestMVCDay1/LittleGameAirGuess/Program.cs
@@ -6,6 +6,9 @@ using System.Threading.Tasks;
 
 namespace LittleGameAirGuess
 {
+
+
+
     class Program
     {
         static int[] Maps = new int[100];
@@ -13,7 +16,7 @@ namespace LittleGameAirGuess
         static int[] Players = new int[2];
 
         static string[] PlayersName = new string[2];
-
+        static bool[] Flag = new bool[2];
         static void Main(string[] args)
         {
 
@@ -52,6 +55,37 @@ namespace LittleGameAirGuess
             Console.WriteLine("{0}的士兵用A表示", PlayersName[1]);
             InitailMap();
             DrowMap();
+
+            while (Players[0]<99&&Players[1]<99)
+            {
+                if (Flag[0]==false)
+                {
+                    PlayGame(0);
+                }
+                else
+                {
+                    Flag[0] = false;
+                }
+                if (Players[0]>=99)
+                {
+                    Console.WriteLine("玩家{0}无耻的赢了玩家{1}", PlayersName[0], PlayersName[1]);
+                    break;
+                }
+                if (Flag[1] == false)
+                {
+                    PlayGame(1);
+                }
+                else
+                {
+                    Flag[1] = false;
+                }
+                if (Players[1] >= 99)
+                {
+                    Console.WriteLine("玩家{0}无耻的赢了玩家{1}", PlayersName[1], PlayersName[0]);
+                    break;
+                }
+            }
+            Win();
             Console.ReadLine();
 
 
@@ -157,6 +191,12 @@ namespace LittleGameAirGuess
         }
 
         #endregion
+
+        /// <summary>
+        /// 抽象出来的画地图的方法，返回一个要画的字符串
+        /// </summary>
+        /// <param name="i">当前地图坐标</param>
+        /// <returns></returns>
         private static string DrowingMap(int i)
         {
             
@@ -200,6 +240,138 @@ namespace LittleGameAirGuess
                 }
             }
             return str;
+        }
+
+        public static void PlayGame(int playerNumber)
+        {
+            Console.WriteLine("玩家{0}按任意键开始掷骰子", PlayersName[playerNumber]);
+            Console.ReadKey(true);//按下的任意键不在控制台显示
+            Random rd = new Random();
+            int rdnumber = rd.Next(1, 7);
+            Console.WriteLine("玩家{0}掷出了{1}", PlayersName[playerNumber], rdnumber);
+            Players[playerNumber] += rdnumber;
+            ChangePos();
+            Console.ReadKey(true);
+            Console.WriteLine("玩家{0}按任意键开始行动", PlayersName[playerNumber]);
+            Console.ReadKey(true);
+            Console.WriteLine("玩家{0}行动完了", PlayersName[playerNumber]);
+            Console.ReadKey(true);
+            //如果玩家A踩到玩家B
+            //    游戏规则：
+            //如果玩家A踩到了玩家B 玩家B退6格
+            //踩到了地雷 退6格
+            //踩到了时空隧道 进10格
+            //踩到了幸运轮盘 1交换位置  2 轰炸对方 使对方退6格
+            //踩到了暂停 暂停一回合 3
+            //踩到了方块 神马都不干 0
+            if (Players[playerNumber] == Players[1 - playerNumber])
+            {
+                Console.WriteLine("玩家{0}踩到了玩家{1},玩家{2}退六格", PlayersName[playerNumber], PlayersName[1- playerNumber], PlayersName[playerNumber]);
+                Players[1] -= 6;
+                ChangePos();
+            }
+            else
+            {
+                switch (Maps[Players[playerNumber]])
+                {
+                    case 0:
+                        Console.WriteLine("玩家{0}踩到了方块什么都不发生", PlayersName[playerNumber]);
+                        Console.ReadKey();
+                        break;
+                    case 1:
+                        Console.WriteLine("玩家{0}踩到了幸运轮盘,1--交换位置,2--轰炸对方 使对方退6格", PlayersName[playerNumber]);
+                        string input = Console.ReadLine();
+                        while (true)
+                        {
+                            if (input == "1")
+                            {
+                                Console.WriteLine("玩家{0}与玩家{1}选择1--交换位置", PlayersName[playerNumber], PlayersName[1 - playerNumber]);
+                                int temp = Players[playerNumber];
+                                Players[playerNumber] = Players[1 - playerNumber];
+                                Players[1 - playerNumber] = temp;
+                                ChangePos();
+                                Console.ReadKey(true);
+                                break;
+                            }
+                            else if (input == "2")
+                            {
+                                Console.WriteLine("玩家{0}选择轰炸对方{1}退6格", PlayersName[playerNumber], PlayersName[1 - playerNumber]);
+                                Players[1 - playerNumber] -= 6;
+                                ChangePos();
+                                Console.ReadKey(true);
+                                break;
+                            }
+                            else
+                            {
+                                Console.WriteLine("玩家{0}踩到了幸运轮盘,1--交换位置,2 轰炸对方 使对方退6格", PlayersName[playerNumber]);
+                                input = Console.ReadLine();
+                            }
+                        }
+                        break;
+                    case 2:
+                        Console.WriteLine("玩家{0}踩到地雷玩家{1}退六格", PlayersName[playerNumber], PlayersName[playerNumber]);
+                        Players[playerNumber] -= 6;
+                        ChangePos();
+                        Console.ReadKey(true);
+                        break;
+                    case 3:
+                        Console.WriteLine("玩家{0}踩到暂停", PlayersName[playerNumber], PlayersName[playerNumber]);
+                        Flag[playerNumber] = true;
+                        Console.ReadKey(true);
+                        break;
+                    case 4:
+                        Console.WriteLine("玩家{0}踩到时空隧道近十格", PlayersName[playerNumber]);
+                        Players[playerNumber] += 10;
+                        ChangePos();
+                        Console.ReadKey(true);
+                        break;
+
+                }
+
+            }
+            Console.Clear();
+            DrowMap();
+        }
+
+        public static void ChangePos()
+        {
+            if (Players[0]<=0)
+            {
+                Players[0] = 0;
+            }
+            if(Players[0] >= 99)
+            {
+                Players[0] = 99;
+            }
+            if (Players[1] <= 0)
+            {
+                Players[1] = 0;
+            }
+            if (Players[1] >= 99)
+            {
+                Players[1] = 99;
+            }
+        }
+        /// <summary>
+        /// 胜利啦
+        /// </summary>
+        public static void Win()
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("                                          ◆                      ");
+            Console.WriteLine("                    ■                  ◆               ■        ■");
+            Console.WriteLine("      ■■■■  ■  ■                ◆■         ■    ■        ■");
+            Console.WriteLine("      ■    ■  ■  ■              ◆  ■         ■    ■        ■");
+            Console.WriteLine("      ■    ■ ■■■■■■       ■■■■■■■   ■    ■        ■");
+            Console.WriteLine("      ■■■■ ■   ■                ●■●       ■    ■        ■");
+            Console.WriteLine("      ■    ■      ■               ● ■ ●      ■    ■        ■");
+            Console.WriteLine("      ■    ■ ■■■■■■         ●  ■  ●     ■    ■        ■");
+            Console.WriteLine("      ■■■■      ■             ●   ■   ■    ■    ■        ■");
+            Console.WriteLine("      ■    ■      ■            ■    ■         ■    ■        ■");
+            Console.WriteLine("      ■    ■      ■                  ■               ■        ■ ");
+            Console.WriteLine("     ■     ■      ■                  ■           ●  ■          ");
+            Console.WriteLine("    ■    ■■ ■■■■■■             ■              ●         ●");
+            Console.ResetColor();
         }
     }
 }
